94695d89b403257ba998a8c3c9f3ff0c5a787015
Michael Mysinger <mysinger@keiserlab.org>
2015-09-13 21:50:26 -0700

    Working dual SEA and EF heatmap

--- seaware/ef_sea_heatmap.py
+++ seaware/ef_sea_heatmap.py
@@ -27,8 +27,9 @@ sys.path.append(labware_path)
 from libraries.lab_utils import ScriptError, gopen
 from sea_orig.clustering import hierarchical_x, hierarchical_y, hierarchical_both
 
-QVALUE_CUTOFF = 0.001
-EF_CUTOFF = 6.0
+QVALUE_CUTOFF = 1.0e-3
+EF_CUTOFF = 5.0
+EVALUE_CUTOFF = 1.0e-12
 
 
 def build_array(value_dict, x_labels, y_labels):
@@ -36,6 +37,25 @@ def build_array(value_dict, x_labels, y_labels):
     for y_label in y_labels:
         values.append([value_dict.get((x_label, y_label), 0.0)
                             for x_label in x_labels])
+    return numpy.array(values)
+
+
+def summed_array(value_dicts, x_labels, y_labels):
+    values = []
+    for y_label in y_labels:
+        values.append([sum(value_dict.get((x_label, y_label), 0.0) 
+            for value_dict in value_dicts) for x_label in x_labels])
+    return numpy.array(values)
+
+
+def joined_array(value_dicts, x_labels, y_labels):
+    values = []
+    for y_label in y_labels:
+        y_values = []
+        for x_label in x_labels:
+            for value_dict in value_dicts:
+                y_values.append(value_dict.get((x_label, y_label), 0.0))
+        values.append(y_values)
     return numpy.array(values)
 
 
@@ -50,13 +70,10 @@ def plot_heatmap(xy_data, out_fn, x_labels=None, y_labels=None):
         x_labels = ""
     if y_labels == None:
         y_labels = ""
-    seaborn.heatmap(xy_data, vmin=3.0, vmax=50.0, square=True, ax=axes, 
+    seaborn.heatmap(xy_data, vmin=-1.0, vmax=1.0, square=True, ax=axes, 
                     xticklabels=x_labels, yticklabels=y_labels, 
-                    cmap="Blues", linewidth=0.1, linecolor="#f0f0ff", 
-                    cbar_kws={"aspect": 40})
-    cbar_axes = figure.axes[-1]
-    cbar_axes.tick_params(labelsize=20)
-    cbar_axes.set_xlabel("EF", size=24)
+                    cmap="RdBu", linewidth=0.1, linecolor="#f0f0ff", 
+                    cbar=False)
     pyplot.title("Enrichment Factor Heatmap", fontsize=24, y=1.01)
     pyplot.savefig(out_fn, bbox_inches="tight", pad_inches=0.3)
 
@@ -64,7 +81,7 @@ def plot_heatmap(xy_data, out_fn, x_labels=None, y_labels=None):
 def read_ef(ef_csv):
     ef_dict = {}
     header = ef_csv.next()
-    logging.info("Skipping input header: %s" % str(header))
+    logging.info("Skipping EF input header: %s" % str(header))
     for row in ef_csv:
         tid, tname, event, ef, pvalue, qvalue = row
         ef = float(ef)
@@ -74,46 +91,117 @@ def read_ef(ef_csv):
             continue
         if not tname.endswith("_HUMAN"):
             continue
+        if event.startswith("cluster_"):
+            continue
         ef_dict[(event, tname)] = ef
+    logging.info("Read %d enrichment factors" % len(ef_dict))
     return ef_dict 
 
-def normalize_ef(ef_dict):
-    pass
 
 def read_sea(sea_csv):
-    pass
+    sea_dict = {}
+    header = sea_csv.next()
+    logging.info("Skipping SEA input header: %s" % str(header))
+    for row in sea_csv:
+        query_id, reference_id, evalue, maxtc = row
+        evalue = float(evalue)
+        if evalue > EVALUE_CUTOFF:
+            continue
+        try:
+            target_id, affinity = reference_id.rsplit("_", 1)
+        except ValueError:
+            target_id = reference_id
+            affinity = None
+        if not target_id.endswith("_HUMAN"):
+            continue
+        sea_dict[(query_id, target_id)] = -numpy.log10(evalue)
+    logging.info("Read %d SEA e-values" % len(sea_dict))
+    return sea_dict
 
-def normalize_sea(sea_dict):
-    pass
 
-def dual_heatmap(ef_csv, sea_csv, out_fn, alphabetical=False):
-    """Plot heatmap of EF data."""
-    value_dict = read_ef(ef_csv)
-    event_ids = list(set(event for event, tname in value_dict.iterkeys()))
-    logging.info("Read in %d event ids" % len(event_ids))
-    target_ids = list(set(tname for event, tname in value_dict.iterkeys()))
-    target_ids.sort()
-    logging.info("Read in %d target ids" % len(target_ids))
-    evalue_data = build_array(value_dict, event_ids, target_ids)
+def normalize(in_dict, lower=0.0, percentile=90.0):
+    """Normalize dictionary values from 0 to 1 (-1 if inverted)"""
+    upper = numpy.percentile(in_dict.values(), percentile)
+    height = upper - lower
+    out_norm = {}
+    for k, v in in_dict.iteritems():
+        if v >= upper:
+            norm = 1.0
+        elif v <= lower:
+            norm = 0.0
+        else:
+            norm = (v - lower)/height
+        out_norm[k] = norm
+    return out_norm, (lower, upper)
+
+
+def invert(in_dict):
+    return dict((k, -v) for k, v in in_dict.iteritems())        
+
+
+def dual_heatmap(ef_csv, sea_csv, out_fn, alphabetical=False, clusters="both"):
+    """Plot heatmap of EF data.
+
+    clusters = both | sea | ef
+    """
+    ef_dict = read_ef(ef_csv)
+    ef_norm, limits = normalize(ef_dict, lower=3.0)
+    logging.info("EFs normalized from %g to %g" % limits) 
+    ef_events = set(event for event, target in ef_norm.iterkeys())
+    ef_targets = set(target for event, target in ef_norm.iterkeys())
+    logging.info("Found %d event and %d target labels in EF data" % 
+                 (len(ef_events), len(ef_targets)))
+    sea_dict = read_sea(sea_csv)
+    sea_norm, limits = normalize(sea_dict, lower=6.0)
+    logging.info("SEA -log10 p-values normalized from %g to %g" % limits) 
+    sea_events = set(event for event, target in sea_norm.iterkeys())
+    sea_targets = set(target for event, target in sea_norm.iterkeys())
+    logging.info("Found %d event and %d target labels in SEA data" % 
+                 (len(sea_events), len(sea_targets)))
+    event_ids = list(ef_events | sea_events)
+    target_ids = list(ef_targets | sea_targets)
+    logging.info("Totaled %d event and %d target labels" % 
+                 (len(event_ids), len(target_ids)))
+    if clusters.lower() == "both":
+        logging.info("Clustering using both EF and SEA results")
+        evalue_data = summed_array([sea_norm, ef_norm], event_ids, target_ids)
+    elif clusters.lower() == "sea":
+        logging.info("Clustering using SEA results only")
+        evalue_data = build_array(sea_dict, event_ids, target_ids)
+    elif clusters.lower() == "ef":
+        logging.info("Clustering using EF results only")
+        evalue_data = build_array(ef_dict, event_ids, target_ids)
+    else:
+        raise ValueError("Unrecognized clusters parameter %s" % clusters)
     clustered_event_ids = hierarchical_x(evalue_data, event_ids, 
                                          threshold=0.5)
     clustered_target_ids = hierarchical_y(evalue_data, target_ids, 
                                           threshold=0.9)
     graph_target_ids = clustered_target_ids
     if alphabetical:
+        logging.info("Showing alphabetical target list instead of clusters")
+        target_ids.sort()
         graph_target_ids = target_ids
-    evalue_data = build_array(value_dict, clustered_event_ids,
-                              graph_target_ids)
-    plot_heatmap(evalue_data, out_fn, x_labels=clustered_event_ids,
+    ef_norm = invert(ef_norm)
+    evalue_data = joined_array([sea_norm, ef_norm], 
+                               clustered_event_ids, graph_target_ids)
+    graph_event_ids = []
+    for event in clustered_event_ids:
+        graph_event_ids.append(event + " sea")
+        graph_event_ids.append(event + " ef")
+    plot_heatmap(evalue_data, out_fn, x_labels=graph_event_ids,
                  y_labels=graph_target_ids)
 
 
 def handler(ef_fn, sea_fn, out_fn, **kwargs):
     """I/O handling for the script."""
+    logging.info("EF input file: %s" % ef_fn)
     ef_f = gopen(ef_fn)
     ef_csv = csv.reader(ef_f)
+    logging.info("SEA input file: %s" % sea_fn)
     sea_f = gopen(sea_fn)
     sea_csv = csv.reader(sea_f)
+    logging.info("Output file: %s" % out_fn)
     try:
         try:
             dual_heatmap(ef_csv, sea_csv, out_fn, **kwargs)
@@ -121,26 +209,40 @@ def handler(ef_fn, sea_fn, out_fn, **kwargs):
             logging.error(message)
             return message.value
     finally:
-        in_f.close()
+        ef_f.close()
+        sea_f.close()
     return 0
+
+
+def add_file_logger(filename, level=logging.INFO,
+                    format="%%(levelname)s: %%(message)s",
+                    logger=logging.getLogger()):
+    file_handler = logging.FileHandler(filename, mode="w")
+    log_formatter = logging.Formatter(format)
+    file_handler.setFormatter(log_formatter)
+    file_handler.setLevel(level)
+    logger.addHandler(file_handler)
 
 
 def main(argv):
     """Parse arguments."""
-    logging.basicConfig(level=logging.INFO,
-                        format="%(levelname)s: %(message)s")
+    log_level = logging.INFO
+    log_format = "%(levelname)s: %(message)s"
+    logging.basicConfig(level=log_level, format=log_format)
     description = "Plot heatmap for EF analysis."
     parser = ArgumentParser(description=description)
     parser.add_argument("ef_file", 
-                        help="input EF analysis CSV file")
+                        help="EF analysis qvalues CSV file")
     parser.add_argument("sea_file", 
-                        help="input SEA results CSV file")
+                        help="SEA scores CSV file")
     parser.add_argument("outfile", 
                         help="output heatmap PNG file")
     parser.add_argument("-a", "--alphabetical", action="store_true",  
                         help="Arrange targets in alphabetical " +
                              "instead of clustured order")
     options = parser.parse_args(args=argv[1:])
+    log_fn = options.outfile.replace(".png", "") + ".log"
+    add_file_logger(log_fn, level=log_level, format=log_format)
     return handler(ef_fn=options.ef_file, sea_fn=options.sea_file, 
                    out_fn=options.outfile, 
                    alphabetical=options.alphabetical)
